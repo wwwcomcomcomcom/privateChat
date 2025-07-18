e1ba8935d5aea231f80c7da4c531a5f513ee9f55
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-07-18 18:57:19 +0900

    Project init

---
+++ src/test/java/privatechat/privatechat/PrivateChatApplicationTests.java
@@ -0,0 +1,13 @@
+package privatechat.privatechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
