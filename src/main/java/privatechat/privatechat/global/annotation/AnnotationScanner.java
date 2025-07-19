62988cf2858fc52888a8f8fefbd22896dfeaaa6d
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-07-19 11:30:07 +0900

    update(global): @SocketMapping을 netty-socketio의 @OnEvent로 대체

--- src/main/java/privatechat/privatechat/global/annotation/AnnotationScanner.java
+++ src/main/java/privatechat/privatechat/global/annotation/AnnotationScanner.java
@@ -26,34 +26,7 @@ public class AnnotationScanner  {
         Map<Object, List<Method>> result = new HashMap<>();
         Map<String, Object> socketControllers = scanSocketController();
         for(Object controller : socketControllers.values()){
-            Class<?> controllerClass = controller.getClass();
-            for(Method method : controllerClass.getMethods()){
-                if(method.isAnnotationPresent(SocketMapping.class)){
-                    Object bean = applicationContext.getBean(controllerClass);
-                    log.info(method.getAnnotation(SocketMapping.class).value());
-                    server.addEventListener(
-                            method.getAnnotation(SocketMapping.class).value(),
-                            method.getParameterTypes()[1],
-                            (socketIOClient, o, ackRequest) -> {
-                                System.out.println(o);
-                                try{
-                                    if (method.getParameterCount() == 2) {
-                                        method.invoke(bean,socketIOClient, o);
-                                    } else if (method.getParameterCount() == 3) {
-                                        method.invoke(bean,socketIOClient, o, ackRequest);
-                                    } else {
-                                        log.error("Method {} has too many parameters: {}", method.getName(), method.getParameterCount());
-                                    }
-                                } catch (InvocationTargetException innerError){
-                                    if(innerError.getTargetException() instanceof Exception){
-                                        throw (Exception) innerError.getTargetException();
-                                    } else {
-                                        log.error("Exception occurred while invoking method: {}", method.getName(), innerError);
-                                    }
-                                }
-                            });
-                }
-            }
+            server.addListeners(controller);
         }
     }
 }
