a5c68ccaf3cc299328c6fa8c528b72b592d9fe69
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-07-19 11:34:48 +0900

    add(chat): 임시 테스트용 코드

---
+++ src/main/java/privatechat/privatechat/domain/chat/ChatListener.java
@@ -0,0 +1,14 @@
+package privatechat.privatechat.domain.chat;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.annotation.OnEvent;
+import org.springframework.stereotype.Component;
+import privatechat.privatechat.global.annotation.SocketListener;
+
+@SocketListener
+@Component
+public class ChatListener {
+    @OnEvent("message")
+    public void chatting(SocketIOClient client, String message){
+        System.out.println(message);
+    }
+}
