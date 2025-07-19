0933650603c9c9a1b8c76e13c68b75e40609b052
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-07-19 11:33:12 +0900

    chore: 미사용 import 삭제

--- src/main/java/privatechat/privatechat/global/annotation/AnnotationScanner.java
+++ src/main/java/privatechat/privatechat/global/annotation/AnnotationScanner.java
@@ -6,9 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Component
