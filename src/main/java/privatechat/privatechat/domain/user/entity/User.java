e9811cba3a4f5125f86203d6a99b6efbb3f5da1e
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-07-22 00:03:35 +0900

    add(user): User JPA 엔티티 추가

---
+++ src/main/java/privatechat/privatechat/domain/user/entity/User.java
@@ -0,0 +1,24 @@
+package privatechat.privatechat.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+}
